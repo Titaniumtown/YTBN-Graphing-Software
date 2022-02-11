23a38b0c454c49968a499f2a8364e8237db57c09
Simon Gardling <titaniumtown@gmail.com>
2022-02-11 13:46:18 -0500

    improve push script

--- push.sh
+++ push.sh
@@ -4,8 +4,9 @@ set -e #kill script if error occurs
 wasm-pack build --target web --release
 
 rm -fr tmp | true #delete tmp folder if exists
-mkdir tmp
-cp -r pkg www tmp/
+mkdir tmp tmp/pkg tmp/www
+cp -r pkg/integral_site_bg.wasm pkg/integral_site_bg.wasm.d.ts pkg/integral_site.d.ts pkg/integral_site.js tmp/pkg/
+cp www/bootstrap.js www/index.html www/index.js www/style.css tmp/www/
 mv tmp/www/index.html tmp/
 
 sed -i 's/style.css/www\/style.css/g' tmp/index.html
