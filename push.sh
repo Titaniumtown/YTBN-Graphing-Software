a63c2369a0a218747fbd1cfbe51ebde76c6e5377
Simon Gardling <titaniumtown@gmail.com>
2022-02-14 13:31:43 -0500

    update push script

--- push.sh
+++ push.sh
@@ -6,13 +6,11 @@ wasm-pack build --target web --release
 rm -fr tmp | true #delete tmp folder if exists
 mkdir tmp tmp/pkg tmp/www
 cp -r pkg/integral_site_bg.wasm pkg/integral_site_bg.wasm.d.ts pkg/integral_site.d.ts pkg/integral_site.js tmp/pkg/
-cp www/bootstrap.js www/index.html www/index.js www/style.css tmp/www/
-mv tmp/www/index.html tmp/
+cp www/bootstrap.js www/index.html www/index.js www/style.css tmp/
+# mv tmp/www/index.html tmp/
 
-sed -i 's/style.css/www\/style.css/g' tmp/index.html
-sed -i 's/bootstrap.js/www\/bootstrap.js/g' tmp/index.html
-
+sed -i 's/\.\.\/pkg/\.\/pkg/g' tmp/bootstrap.js
 
 echo "rsyncing"
-rsync -av --delete --info=progress2 tmp/ rpi-public:/mnt/hdd/http_share/integrals/
+rsync -av --delete --info=progress2 tmp/ rpi-public:/mnt/hdd/http_share/integral-demo/
 rm -fr tmp
