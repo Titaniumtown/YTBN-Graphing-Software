30dfcfa36a65f6d9b3d13832caccef7c34fbbdb5
Simon Gardling <titaniumtown@gmail.com>
2022-02-17 09:16:11 -0500

    update

--- push.sh
+++ push.sh
@@ -13,4 +13,9 @@ sed -i 's/\.\.\/pkg/\.\/pkg/g' tmp/bootstrap.js
 
 echo "rsyncing"
 rsync -av --delete --info=progress2 tmp/ rpi-public:/mnt/hdd/http_share/integral-demo/
+rsync -av --delete --info=progress2 --exclude=".git" tmp/ ../integral-demo/
 rm -fr tmp
+cd ../integral-demo
+git add .
+git commit -m "update"
+git push
