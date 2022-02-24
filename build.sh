dd59aab748595fa8ea4cf5cea9710e7e29ded7aa
Simon Gardling <titaniumtown@gmail.com>
2022-02-24 11:28:22 -0500

    llvm-strip

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ set -e
 wasm-pack build --target web --release --no-typescript
 wasm-opt -Oz -o pkg/integral_site_bg_2.wasm pkg/integral_site_bg.wasm
 mv pkg/integral_site_bg_2.wasm pkg/integral_site_bg.wasm
+llvm-strip --strip-all pkg/integral_site_bg.wasm
