8b53c7046bc9632bfdfe0da2754472a68f7fffe1
Simon Gardling <titaniumtown@gmail.com>
2022-02-24 21:32:58 -0500

    remove old comment

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ mod misc;
 fn main() {
     let app = egui_app::MathApp::default();
     let options = eframe::NativeOptions {
-        // Let's show off that we support transparent windows
         transparent: true,
         drag_and_drop_support: true,
         ..Default::default()
