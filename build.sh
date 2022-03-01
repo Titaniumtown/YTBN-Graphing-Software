f4a26ff8feef20eb91e2c77c8daf9525ccb3e7ec
Simon Gardling <titaniumtown@gmail.com>
2022-03-01 09:11:18 -0500

    Os
    Os vs Oz, size wise, only is about 100kb difference. but Oz disables some optimizations. Enabling Os instead of Oz brings the binary size up from 876527 bytes to 972775 bytes (a 10.9% increase).
    
    Note: I am referring to wasm binary size as that's the main focus of this project

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 wasm-pack build --target web --release --no-typescript
-wasm-opt -Oz -o pkg/integral_site_bg_2.wasm pkg/integral_site_bg.wasm
+wasm-opt -Os -o pkg/integral_site_bg_2.wasm pkg/integral_site_bg.wasm
 mv pkg/integral_site_bg_2.wasm pkg/integral_site_bg.wasm
 llvm-strip --strip-all pkg/integral_site_bg.wasm
 
