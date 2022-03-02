3918f945a6ce12604e2ac00d84a8b29613393639
Simon Gardling <titaniumtown@gmail.com>
2022-03-02 18:59:37 -0500

    fix?

--- build.rs
+++ build.rs
@@ -1,8 +1,4 @@
-use std::process::Command;
-
-fn main() -> shadow_rs::SdResult<()> {
-    let output = Command::new("git").args(&["rev-parse", "HEAD"]).output().unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
-    println!("cargo:rustc-env=GIT_HASH={}", git_hash);
-    shadow_rs::new()
+fn main() {
+    println!("cargo:rerun-if-changed=.git/refs/heads/main"); // genius
+    shadow_rs::new().unwrap();
 }
