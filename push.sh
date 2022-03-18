17916a2b239a2b154b6fd651dcbddb95041c2353
Simon Gardling <titaniumtown@gmail.com>
2022-03-18 13:41:20 -0400

    update push.sh

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@ set -e #kill script if error occurs
 bash build.sh
 
 echo "rsyncing"
-rsync -av --delete --info=progress2 tmp/ rpi-public:/mnt/hdd/http_share/integral-demo/
+#rsync -av --delete --info=progress2 tmp/ rpi-public:/mnt/hdd/http_share/integral-demo/
 rsync -av --delete --info=progress2 --exclude=".git" tmp/ ../titaniumtown.github.io/
 rm -fr tmp
 cd ../titaniumtown.github.io
