8587dbf78c5833910b4e19da940d9ae73bdfa739
Simon Gardling <titaniumtown@gmail.com>
2022-03-22 11:46:02 -0400

    fix when running natively

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,12 @@ mod parsing;
 // For running the program natively! (Because why not?)
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
+	let subscriber = tracing_subscriber::FmtSubscriber::builder()
+		.with_max_level(tracing::Level::TRACE)
+		.finish();
+
+	tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
 	let options = eframe::NativeOptions {
 		transparent: true,
 		drag_and_drop_support: true,
