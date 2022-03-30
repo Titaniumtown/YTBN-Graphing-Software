dd2b6ce00e8b9cd05297821d706215af5737438f
Simon Gardling <titaniumtown@gmail.com>
2022-03-30 14:34:49 -0400

    update build.rs

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ use std::path::Path;
 fn main() {
 	// rebuild if new commit or contents of `assets` folder changed
 	println!("cargo:rerun-if-changed=.git/logs/HEAD");
-	println!("cargo:rerun-if-changed=assets");
+	println!("cargo:rerun-if-changed=assets/*");
 
 	let _ = command_run::Command::with_args("./pack_assets.sh", &[""])
 		.enable_capture()
