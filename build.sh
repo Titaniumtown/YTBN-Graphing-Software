d1e89f9967ba372e863b32abcad7c2a56623fcd4
Simon Gardling <titaniumtown@gmail.com>
2022-04-26 23:24:53 -0400

    build changes

--- build.sh
+++ build.sh
@@ -11,16 +11,18 @@ wasm_opt() {
     wasm-opt -Oz -o pkg/ytbn_graphing_software_bg_2.wasm pkg/ytbn_graphing_software_bg.wasm
     mv pkg/ytbn_graphing_software_bg_2.wasm pkg/ytbn_graphing_software_bg.wasm
 }
+#export RUSTFLAGS="--cfg=web_sys_unstable_apis"
+export COMMON_PACK="--target web --no-typescript -- -Z build-std=std,panic_abort -C linker-plugin-lto=yes -C inline-threshold=275"
 
 if test "$1" == "" || test "$1" == "release"; then
-    RUSTFLAGS=--cfg=web_sys_unstable_apis wasm-pack build --target web --release --no-typescript
+    wasm-pack build --release
     echo "Binary size (pre-wasm_opt): $(du -sb pkg/ytbn_graphing_software_bg.wasm)"
     wasm_opt #apply wasm optimizations
     echo "Binary size (pre-strip): $(du -sb pkg/ytbn_graphing_software_bg.wasm)"
     llvm-strip --strip-all pkg/ytbn_graphing_software_bg.wasm
     
     elif test "$1" == "debug"; then
-    RUSTFLAGS=--cfg=web_sys_unstable_apis wasm-pack build --target web --dev --no-typescript
+    wasm-pack build --dev $COMMON_PACK
 else
     echo "ERROR: build.sh, argument invalid"
     exit 1
