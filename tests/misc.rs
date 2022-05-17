149adfd614df2071e94fdb1d97c4d1aaa8d35c73
Simon Gardling <titaniumtown@gmail.com>
2022-05-17 09:04:04 -0400

    add test for invalid_hashed_storage

--- tests/misc.rs
+++ tests/misc.rs
@@ -108,3 +108,9 @@ fn hashed_storage() {
 	let read = hashed_storage_read(storage);
 	assert_eq!(read.map(|(a, b)| (a.to_vec(), b)), Some((commit, data)));
 }
+
+#[test]
+fn invalid_hashed_storage() {
+	use ytbn_graphing_software::hashed_storage_read;
+	assert_eq!(hashed_storage_read(String::from("aaaa")), None);
+}
