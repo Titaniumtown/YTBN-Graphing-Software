a382cae9141a9fd147e1ad1e502f0c75580e2bbd
Simon Gardling <titaniumtown@gmail.com>
2022-05-17 18:02:03 -0400

    edit message

--- src/lib.rs
+++ src/lib.rs
@@ -47,10 +47,11 @@ cfg_if::cfg_if! {
 
 		#[wasm_bindgen(start)]
 		pub fn start() -> Result<(), wasm_bindgen::JsValue> {
-			tracing::info!("Initializing...");
+			tracing::info!("Starting...");
+
 			// Used in order to hook into `panic!()` to log in the browser's console
 			tracing_wasm::set_as_global_default();
-			tracing::info!("Starting App...");
+
 			eframe::start_web("canvas", Box::new(|cc| Box::new(math_app::MathApp::new(cc))))
 		}
 	}
