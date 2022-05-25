24526d138daab40ce47a5cdbc851dd359e4f4e14
Simon Gardling <titaniumtown@gmail.com>
2022-05-25 12:10:23 -0400

    widgets_ontop: return InnerResponse<R>

--- src/widgets.rs
+++ src/widgets.rs
@@ -1,12 +1,14 @@
+use egui::InnerResponse;
 use std::hash::Hash;
 
+/// Creates an area ontop of a widget with an y offset
 pub fn widgets_ontop<R>(
 	ui: &egui::Ui, id: impl Hash, re: &egui::Response, y_offset: f32,
 	add_contents: impl FnOnce(&mut egui::Ui) -> R,
-) -> R {
+) -> InnerResponse<R> {
 	let area = egui::Area::new(id)
 		.fixed_pos(re.rect.min.offset_y(y_offset))
 		.order(egui::Order::Foreground);
 
-	area.show(ui.ctx(), |ui| add_contents(ui)).inner
+	area.show(ui.ctx(), |ui| add_contents(ui))
 }
