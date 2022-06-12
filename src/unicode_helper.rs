11464135bc894f0b4951967d3b059ade5df3a09d
Simon Gardling <titaniumtown@gmail.com>
2022-06-12 15:36:43 -0400

    simplify to_chars_array

--- src/unicode_helper.rs
+++ src/unicode_helper.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 #[allow(dead_code)]
 pub fn to_unicode_hash(c: char) -> String {
 	c.escape_unicode()
@@ -15,16 +17,7 @@ pub fn to_chars_array(chars: Vec<char>) -> String {
 		&chars
 			.iter()
 			.map(|c| format!("'{}'", c.escape_unicode()))
-			.enumerate()
-			.map(|(i, x)| {
-				// Add comma and space if needed
-				match chars.len() > i + 1 {
-					true => x + ", ",
-					false => x,
-				}
-			})
-			.collect::<Vec<String>>()
-			.concat(),
+			.join(", "),
 		"]",
 	]
 	.concat()
